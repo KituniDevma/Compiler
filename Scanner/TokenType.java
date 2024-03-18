bff4f568ed7344503624a86030f9ae12e84ede17
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2024-03-18 11:08:05 +0530

    Initial Commit

---
+++ Scanner/TokenType.java
@@ -0,0 +1,17 @@
+
+public enum TokenType {
+    KEYWORD,
+    IDENTIFIER,
+    INTEGER,
+    OPERATOR,
+    STRING,
+    PUNCTUATION,
+    COMMENT,
+    DELETE,
+    L_PAREN,
+    R_PAREN,
+    SEMICOLON,
+    
+    COMMA}
+
+
